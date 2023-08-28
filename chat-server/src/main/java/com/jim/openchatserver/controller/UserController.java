802b23076eb16f478a0d2546b394c4fdcbb2c521
jim <jim125027@gmail.com>
2023-08-28 18:05:38 +0800

    临时提交

---
+++ chat-server/src/main/java/com/jim/openchatserver/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.jim.openchatserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jim
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    /**
+     * 新增用户
+     */
+    @RequestMapping("/add")
+    public void add() {
+
+    }
+}
